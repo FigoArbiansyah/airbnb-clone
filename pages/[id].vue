cf842fd7c78461729aa113362fd7e6f802e8dbc2
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-05-31 01:50:54 +0700

    Halaman Pesan

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main-content">
+  <div class="main-content pb-20">
     <DetailNavbar />
     <main class="min-h-screen">
       <section class="relative mt-5 md:px-[115px] px-3">
@@ -99,9 +99,9 @@
         </div>
       </section>
       <section
-        class="mt-8 grid md:grid-cols-12 grid-cols-1 gap-x-16 md:px-[115px] px-3 relative"
+        class="mt-8 grid md:grid-cols-12 grid-cols-1 gap-16 md:px-[115px] px-3 relative"
       >
-        <div class="md:col-span-7 min-h-[200vh]">
+        <div class="md:col-span-7 md:min-h-[200vh]">
           <div
             class="flex gap-2 w-full justify-between items-center border-b pb-5"
           >
@@ -127,6 +127,90 @@
               />
             </div>
           </div>
+          <div class="mt-5 pb-5 border-b">
+            <div class="flex items-start gap-x-3">
+              <svg
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                role="presentation"
+                focusable="false"
+                class="md:w-1/12 w-[10%] mt-2"
+                style="
+                  display: block;
+                  height: 24px;
+                  width: 24px;
+                  fill: currentcolor;
+                "
+              >
+                <path
+                  d="M26 2a1 1 0 0 1 .922.612l.04.113 2 7a1 1 0 0 1-.847 1.269L28 11h-3v5h6v2h-2v13h-2l.001-2.536a3.976 3.976 0 0 1-1.73.527L25 29H7a3.982 3.982 0 0 1-2-.535V31H3V18H1v-2h5v-4a1 1 0 0 1 .883-.993L7 11h.238L6.086 8.406l1.828-.812L9.427 11H12a1 1 0 0 1 .993.883L13 12v4h10v-5h-3a1 1 0 0 1-.987-1.162l.025-.113 2-7a1 1 0 0 1 .842-.718L22 2h4zm1 16H5v7a2 2 0 0 0 1.697 1.977l.154.018L7 27h18a2 2 0 0 0 1.995-1.85L27 25v-7zm-16-5H8v3h3v-3zm14.245-9h-2.491l-1.429 5h5.349l-1.429-5z"
+                ></path>
+              </svg>
+              <div class="md:w-10/12 w-[80%]">
+                <p class="font-semibold">Area kerja khusus.</p>
+                <p class="text-gray-500 text-sm mt-1.5">
+                  Kamar dengan wifi yang cocok untuk bekerja.
+                </p>
+              </div>
+            </div>
+            <div class="flex items-start mt-4 gap-x-3">
+              <svg
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                role="presentation"
+                focusable="false"
+                class="md:w-1/12 w-[10%] mt-2"
+                style="
+                  display: block;
+                  height: 24px;
+                  width: 24px;
+                  fill: currentcolor;
+                "
+              >
+                <path
+                  d="m24.3334 1.66675c1.0543745 0 1.9181663.81587127 1.9945143 1.85073677l.0054857.14926323-.00065 24.666 3.00065.00075v2h-26.66665v-2l3-.00075v-24.666c0-1.05436681.81587301-1.91816558 1.850737-1.99451429l.149263-.00548571zm-4.00065 2h-12.666l-.00075 24.66625 12.66675-.00025zm4.00065 0h-2.00065v24.666l2.00025.00025zm-7.0001 11.00002c.7363778 0 1.33333.5969522 1.33333 1.33333s-.5969522 1.33333-1.33333 1.33333-1.33333-.5969522-1.33333-1.33333.5969522-1.33333 1.33333-1.33333z"
+                ></path>
+              </svg>
+              <div class="md:w-10/12 w-[80%]">
+                <p class="font-semibold">Check-in mandiri.</p>
+                <p class="text-gray-500 text-sm mt-1.5">
+                  Anda bisa check-in dengan dibantu penjaga pintu.
+                </p>
+              </div>
+            </div>
+            <div class="flex items-start mt-4 gap-x-3">
+              <svg
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                role="presentation"
+                focusable="false"
+                class="md:w-1/12 w-[10%] mt-2"
+                style="
+                  display: block;
+                  height: 24px;
+                  width: 24px;
+                  fill: currentcolor;
+                "
+              >
+                <path
+                  d="m16 17c3.8659932 0 7 3.1340068 7 7s-3.1340068 7-7 7-7-3.1340068-7-7 3.1340068-7 7-7zm0 2c-2.7614237 0-5 2.2385763-5 5s2.2385763 5 5 5 5-2.2385763 5-5-2.2385763-5-5-5zm9.6666667-18.66666667c1.0543618 0 1.9181651.81587779 1.9945142 1.85073766l.0054858.14926234v6.38196601c0 .70343383-.3690449 1.35080636-.9642646 1.71094856l-.1413082.0779058-9.6666667 4.8333334c-.5067495.2533747-1.0942474.2787122-1.6171466.0760124l-.1717078-.0760124-9.66666666-4.8333334c-.62917034-.3145851-1.04315599-.93418273-1.09908674-1.62762387l-.00648607-.16123049v-6.38196601c0-1.05436179.81587779-1.91816512 1.85073766-1.99451426l.14926234-.00548574zm0 2h-19.33333337v6.38196601l9.66666667 4.83333336 9.6666667-4.83333336z"
+                ></path>
+              </svg>
+              <div class="md:w-10/12 w-[80%]">
+                <p class="font-semibold">
+                  Ari & Kim , adalah seorang HosTeladan.
+                </p>
+                <p class="text-gray-500 text-sm mt-1.5">
+                  HosTeladan adalah tuan rumah berpengalaman dan berpenilaian
+                  tinggi yang berkomitmen dalam menyediakan penginapan luar
+                  biasa bagi tamu.
+                </p>
+              </div>
+            </div>
+          </div>
           <div class="mt-5">
             <p class="text-gray-800">
               {{ data?.description }}. Vila mewah baru di Bali Utara dengan
@@ -145,7 +229,7 @@
               <span class="text-xl font-semibold text-gray-700 line-through"
                 >Rp {{ (data.price * 14987).toLocaleString("id-ID") }}
               </span>
-              <span class="text-xl font-semibold">
+              <span class="text-xl font-semibold float-right">
                 Rp
                 {{
                   (
@@ -194,9 +278,9 @@
             </div>
           </div>
           <div class="mt-4 text-center">
-            <NuxtLink to="/reservation"
+            <NuxtLink :to="`/reservation?id=${data.id}`"
               ><div
-                class="w-full text-center py-3 bg-[#EF5A5F] rounded-xl font-semibold text-white"
+                class="w-full text-center py-3 bg-[#EF5A5F] hover:bg-[#FF5F5C] rounded-xl font-semibold text-white transition-all ease"
               >
                 Pesan
               </div></NuxtLink
---
+++ pages/reservation.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="main-content">
+    <ReservationNavbar />
+    <main class="md:py-20 md:pl-24 md:pr-32 max-md:p-5">
+      <div class="relative">
+        <NuxtLink
+          :to="`/${id}`"
+          class="cursor-pointer w-10 h-10 rounded-full grid place-items-center hover:bg-gray-100 absolute top-0 -left-4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#000"
+            class="w-4 h-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
+        </NuxtLink>
+        <p class="text-3xl font-semibold relative left-6">
+          Konfirmasikan dan bayar
+        </p>
+      </div>
+      <div class="md:ml-6 grid grid-cols-2">
+        <div class="pb-8 border-b">
+          <div class="mt-10 py-4 px-5 border rounded-xl relative">
+            <div>
+              <p class="font-semibold">Harga lebih rendah</p>
+              <p class="mt-2 w-[80%]">
+                Harga untuk tanggal pilihan Anda lebih murah Rp3951062 dari
+                harga per malam rata-rata 60 hari terakhir.
+              </p>
+            </div>
+            <svg
+              class="absolute top-5 right-10"
+              viewBox="0 0 48 48"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="presentation"
+              focusable="false"
+              style="
+                display: block;
+                height: 32px;
+                width: 32px;
+                fill: rgb(227, 28, 95);
+                stroke: currentcolor;
+              "
+            >
+              <g stroke="none">
+                <path
+                  d="M24.556 8H9a1 1 0 0 0-.993.883L8 9v15.556a1 1 0 0 0 .206.608l.087.1 17.213 17.213a1 1 0 0 0 1.32.083l.094-.083L42.477 26.92a1 1 0 0 0 .083-1.32l-.083-.094L25.263 8.293a1 1 0 0 0-.575-.284L24.556 8z"
+                  fill-opacity=".2"
+                ></path>
+                <path
+                  d="M24.556 4A5 5 0 0 1 27.9 5.282l.192.182 17.213 17.214a5 5 0 0 1 .172 6.89l-.172.18L29.75 45.306a5 5 0 0 1-6.89.172l-.181-.172L5.464 28.092a5 5 0 0 1-1.457-3.271L4 24.556V9a5 5 0 0 1 4.783-4.995L9 4h15.556zm0 2H9a3 3 0 0 0-2.995 2.824L6 9v15.556a3 3 0 0 0 .743 1.977l.136.145L24.092 43.89a3 3 0 0 0 4.099.135l.144-.135L43.89 28.335a3 3 0 0 0 .135-4.1l-.135-.143L26.678 6.879a3 3 0 0 0-1.924-.872L24.556 6zM13 10a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+                ></path>
+              </g>
+            </svg>
+          </div>
+          <div class="mt-6">
+            <p class="font-semibold text-2xl">Perjalanan Anda</p>
+          </div>
+          <div class="mt-6 flex justify-between">
+            <div>
+              <p class="font-semibold text-lg">Tanggal</p>
+              <p class="">11-14 Jun</p>
+            </div>
+            <div>
+              <p class="cursor-pointer underline font-semibold">Edit</p>
+            </div>
+          </div>
+          <div class="mt-6 flex justify-between">
+            <div>
+              <p class="font-semibold text-lg">Tamu</p>
+              <p class="">2 tamu</p>
+            </div>
+            <div>
+              <p class="cursor-pointer underline font-semibold">Edit</p>
+            </div>
+          </div>
+        </div>
+        <div class="pl-24 mt-10">
+          <div class="py-4 px-5 border rounded-xl">
+            <div class="flex gap-x-3">
+              <div class="">
+                <img
+                  src="~/assets/img/reservation.webp"
+                  class="w-32 aspect-square object-cover rounded-lg"
+                  alt=""
+                />
+              </div>
+              <div class="flex flex-col justify-between">
+                <div>
+                  <p class="text-xs text-gray-800">Seluruh vila</p>
+                  <p class="uppercase text-gray-800">
+                    180° VIEW, PRIVATE POOL VILLA..
+                  </p>
+                </div>
+                <div>
+                  <div class="flex items-center gap-x-1">
+                    <svg
+                      width="14"
+                      height="14"
+                      viewBox="0 0 8 8"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4 0L4.89806 2.76393H7.80423L5.45308 4.47214L6.35114 7.23607L4 5.52786L1.64886 7.23607L2.54692 4.47214L0.195774 2.76393H3.10194L4 0Z"
+                        fill="#000"
+                      />
+                    </svg>
+                    <p class="text-xs text-slate-500">
+                      <span>4.9 . (365)</span>
+                      .
+                    </p>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style="
+                        display: block;
+                        height: 12px;
+                        width: 12px;
+                        fill: currentcolor;
+                      "
+                    >
+                      <path
+                        d="M8.498 7.593l3.1-1.74c.5-.278.99-.552 1.474-.82a.833.833 0 0 0 .428-.729v-2.97A.833.833 0 0 0 12.667.5H3.333a.833.833 0 0 0-.833.833v2.97c0 .303.164.582.428.729l3 1.675 1.575.886c.348.195.647.195.995-.001zM8 8.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7z"
+                      ></path>
+                    </svg>
+                    <span class="text-xs text-slate-500">Hos Teladan</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: null,
+    };
+  },
+  mounted() {
+    this.id = this.$route.query.id;
+  },
+};
+</script>
