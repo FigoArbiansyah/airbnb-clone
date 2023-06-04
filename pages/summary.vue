db0b6e27cc7dc5972c1dcb14dff131d33293ef34
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-06-04 16:52:15 +0700

    Benerin format angka dan juga link saat memesan

--- pages/summary.vue
+++ pages/summary.vue
@@ -146,7 +146,7 @@
             </div>
             <div class="pt-5 flex justify-between font-semibold pb-2">
               <p>Total (IDR)</p>
-              <p>Rp. {{ total.toLocaleString().split(".")[0] }}</p>
+              <p>Rp. {{ total.toLocaleString() }}</p>
             </div>
           </div>
         </div>
@@ -157,10 +157,10 @@
   <div class="py-4 fixed bottom-0 bg-white w-full border-t px-4 z-50">
     <div class="flex justify-between font-semibold ">
       <p>Total (IDR)</p>
-      <p>Rp. {{ total.toLocaleString().split(".")[0] }}</p>
+      <p>Rp. {{ total.toLocaleString() }}</p>
     </div>
     <div class="text-end mt-6">
-      <a :href="`https://wa.me/0895358905664?text=${message}`" class="py-2 px-4 bg-[#EF5A5F] hover:bg-[#FF5F5C] rounded-lg font-semibold text-white transition-all ease" target="_blank">Pesan Sekarang</a>
+      <a :href="`https://wa.me/+62895358905664?text=${message}`" class="py-2 px-4 bg-[#EF5A5F] hover:bg-[#FF5F5C] rounded-lg font-semibold text-white transition-all ease" target="_blank">Pesan Sekarang</a>
     </div>
   </div>
 </template>
