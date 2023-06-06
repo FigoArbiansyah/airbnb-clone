76d979d09f239b6ae8cbfdf22e7525818a4ac6ff
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-06-06 17:57:34 +0700

    Summary not found

--- pages/summary.vue
+++ pages/summary.vue
@@ -1,5 +1,6 @@
 <template>
-  <NuxtLayout name="summary-layout">
+  <div class="main-content">
+    <ReservationNavbar />
     <main class="md:py-20 md:pl-24 md:pr-32 max-md:p-5 max-md:pb-32 max-md:overflow-x-hidden">
       <div class="relative">
         <NuxtLink
@@ -151,7 +152,17 @@
         </div>
       </div>
     </main>
-  </NuxtLayout>
+  </div>
+  <!-- TOTAL FIXED -->
+  <div class="py-4 fixed bottom-0 bg-white w-full border-t px-4 z-50">
+    <div class="flex justify-between font-semibold ">
+      <p>Total (IDR)</p>
+      <p>Rp. {{ total.toLocaleString() }}</p>
+    </div>
+    <div class="text-end mt-6">
+      <a :href="`https://wa.me/+62895358905664?text=${message}`" class="py-2 px-4 bg-[#EF5A5F] hover:bg-[#FF5F5C] rounded-lg font-semibold text-white transition-all ease" target="_blank">Pesan Sekarang</a>
+    </div>
+  </div>
 </template>
 
 <script>
